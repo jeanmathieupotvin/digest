ff642b6769657ce0d4b0fa60eb5081b492e4a4ba
Jean-Mathieu Potvin <jm@potvin.xyz>
2021-04-13 21:05:44 -0400

    Initial commit on unit tests for class Food.

--- tests/test.js
+++ tests/test.js
@@ -10,13 +10,43 @@
 "use strict";
 
 // Dependencies.
-const assert = require("assert").strict;
-const digest = require("../");
+const assert = require('assert').strict;
+const { Food, FoodCollection, FoodQuery } = require('../');
 
 // Load test dataset.
-const foodArr = require('test-data.json');
+const foodArr = require('./test-data');
 
 // Construct a FoodCollection instance from test data.
-const foodCollection = new digest.FoodCollection(foodArr);
+const foodColl = new FoodCollection(foodArr);
 
-// TBD.
+/*!
+ * =============================================================================
+ * Class Food
+ * =============================================================================
+ */
+
+describe('check Food class', function() {
+    it('constructor returns a Food object', function() {
+        assert.strictEqual(new Food(foodArr[0]) instanceof Food, true);
+    });
+    
+    it('.validate() flags non-string properties', function() {
+        const foodErr = Object.assign(foodArr[0]);
+        foodErr.foodEn = false;
+        assert.throws(() => new Food(foodErr));
+    });
+
+    it('.validate() flags non-standard food categories', function() {
+        const foodErr = Object.assign(foodArr[0]);
+        
+        // Insert error into property catJm and test.
+        foodErr.catJm = 'error';
+        assert.throws(() => new Food(foodErr));
+
+        // Remove error from catJm, insert
+        // error into property catRen and test.
+        foodErr.catJm  = foodErr.catRen;
+        foodErr.catRen = 'error';
+        assert.throws(() => new Food(foodErr));
+    });
+});
